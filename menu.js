6fd438d5f8ac1149f7ad6093ba4c3e29737171d8
Romalia23 <79064452+Romalia23@users.noreply.github.com>
2026-09-23 07:37:55 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,18 +1,13 @@
 $(document).ready(function () {
-
-  // Get the filename of the page currently open.
   let currentPage = window.location.pathname.split("/").pop();
 
-  // GitHub Pages may open the homepage without showing index.html.
   if (currentPage === "") {
     currentPage = "index.html";
   }
 
-  // Load the navigation information from menu.json.
+  // menu.json is in the same folder as the HTML pages
   $.getJSON("menu.json")
     .done(function (data) {
-
-      // Begin creating the navigation.
       let menuHTML = `
         <nav class="main-menu">
           <div class="website-name">
@@ -22,17 +17,13 @@ $(document).ready(function () {
           <div class="menu-links">
       `;
 
-      // Repeat these instructions for every item in menu.json.
       data.menuItems.forEach(function (item) {
-
-        // The active class will highlight the current page.
         let activeClass = "";
 
         if (item.link === currentPage) {
           activeClass = "active";
         }
 
-        // Create a navigation link using the JSON information.
         menuHTML += `
           <a href="${item.link}" class="${activeClass}">
             ${item.name}
@@ -40,23 +31,20 @@ $(document).ready(function () {
         `;
       });
 
-      // Finish the navigation HTML.
       menuHTML += `
           </div>
         </nav>
       `;
 
-      // Display the finished navigation inside menu-container.
       $("#menu-container").html(menuHTML);
     })
+    .fail(function (jqxhr, textStatus, error) {
+      console.error("Menu error:", textStatus, error);
 
-    // Display an error if menu.json cannot be loaded.
-    .fail(function () {
       $("#menu-container").html(`
         <p class="menu-error">
           The navigation menu could not be loaded.
         </p>
       `);
     });
-
 });
